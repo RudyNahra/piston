50e2a5c19366818d7ab5097781706ada0cd26699
guipenedo <nostrumg@gmail.com>
2025-02-25 14:49:01 +0100

    env updates

--- packages/cms_ioi/1.0.0/build.sh
+++ packages/cms_ioi/1.0.0/build.sh
@@ -6,22 +6,34 @@ PREFIX=$(realpath $(dirname $0))
 
 mkdir -p build obj
 
+# Download both tarballs first
 cd build
-
 curl "https://ftp.gnu.org/gnu/gcc/gcc-10.2.0/gcc-10.2.0.tar.gz" -o gcc.tar.gz
+curl -L "https://github.com/jqlang/jq/releases/download/jq-1.7.1/jq-1.7.1.tar.gz" -o jq.tar.gz
 
-tar xzf gcc.tar.gz --strip-components=1
+# Install jq
+mkdir jq
+cd jq
+tar xzf ../jq.tar.gz --strip-components=1
+autoreconf -fi
+./configure --prefix="$PREFIX"
+make -j$(nproc)
+make install -j$(nproc)
+cd ..
 
+# Install gcc
+mkdir gcc
+cd gcc
+tar xzf ../gcc.tar.gz --strip-components=1
 ./contrib/download_prerequisites
+cd ..
 
 cd ../obj
 
 # === autoconf based === 
-../build/configure --prefix "$PREFIX" --enable-languages=c,c++,d,fortran --disable-multilib --disable-bootstrap
+../build/gcc/configure --prefix "$PREFIX" --enable-languages=c,c++,d,fortran --disable-multilib --disable-bootstrap
 
 make -j$(nproc)
 make install -j$(nproc)
 cd ../
 rm -rf build obj
-
-
